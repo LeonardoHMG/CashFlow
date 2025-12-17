83eb403f9afb287d57cc105addf6561d1f180afd
Leonardo Gussi <leonardohmgussi@gmail.com>
2025-12-17 17:12:04 -0300

    feat: adding usecase to register expense

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/ReigsterExpenseUseCase.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class ReigsterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        // TO DO VALIDATIONS
+
+        return new ResponseRegisterExpenseJson();
+    }
+}
