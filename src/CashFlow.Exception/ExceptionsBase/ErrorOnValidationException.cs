c1bf5d83d2a751b1f38767416bc3d71729d28424
Leonardo Gussi <leonardohmgussi@gmail.com>
2026-02-14 13:30:52 -0300

    feat: using open closed principe for exceptions

--- src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
+++ src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,9 +1,19 @@
-﻿namespace CashFlow.Exception.ExceptionsBase;
+﻿using System.Net;
+
+namespace CashFlow.Exception.ExceptionsBase;
 public class ErrorOnValidationException: CashFlowException
 {
-    public List<string> Erros { get; init; }
+    private readonly List<string> _errors;
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
     public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
     {
-        Erros = errorMessages;
+        _errors = errorMessages;
+    }
+
+    public override List<string> GetErrors()
+    {
+        return _errors;
     }
 }
