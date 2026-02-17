cbd020771f6db74d7477697b8be580691b150f43
Leonardo Gussi <leonardohmgussi@gmail.com>
2026-02-17 11:45:16 -0300

    feat: creating excel file and setting the fonts

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -10,9 +11,11 @@ public class ReportController : ControllerBase
     [HttpGet("excel")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> GetExcel([FromHeader] DateOnly month)
+    public async Task<IActionResult> GetExcel(
+        [FromServices] IGenerateExpensesReportExcelUseCase useCase,
+        [FromHeader] DateOnly month)
     {
-        byte[] file = new byte[1];
+        byte[] file = await useCase.Execute(month);
 
         if (file.Length > 0)
             return File(file, MediaTypeNames.Application.Octet, "report.xlsx"); 
