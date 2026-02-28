5209de0084ec20b134e601b5a3d8e35900f6e73d
Leonardo <leonardohmgussi@gmail.com>
2026-02-28 10:07:33 -0300

    feat: adding document

--- src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
+++ src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace CashFlow.Domain.Reports {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ResourceReportGenerationMessages {
@@ -129,6 +129,15 @@ namespace CashFlow.Domain.Reports {
         public static string ELETRONIC_TRANSFER {
             get {
                 return ResourceManager.GetString("ELETRONIC_TRANSFER", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Expenses for.
+        /// </summary>
+        public static string EXPENSES_FOR {
+            get {
+                return ResourceManager.GetString("EXPENSES_FOR", resourceCulture);
             }
         }
         
