87e5d9b6473110fa26bdca19481fda7ec04f1199
Leonardo <leonardohmgussi@gmail.com>
2026-05-01 12:43:58 -0300

    tests: starting usecases test

---
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace UseCases.Test.Users.Register;
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var resquest = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(resquest);
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(resquest.Name);
+        result.Token.ShouldNotBeNullOrWhiteSpace();
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        return new RegisterUserUseCase(null, null, null, null, null, null);
+    }
+}
