682819619c02474da36b1852e7575beb2db09a28
Leonardo <leonardohmgussi@gmail.com>
2026-05-09 14:39:04 -0300

    feat: finishing unit test for login usecase

--- tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
+++ tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
@@ -17,8 +17,9 @@ public class DoLoginUseCaseTest
         var user = UserBuilder.Build();
 
         var request = RequestLoginJsonBuilder.Build();
+        request.Email = user.Email;
 
-        var useCase = CreateUseCase(user);
+        var useCase = CreateUseCase(user, request.Password);
 
         var result = await useCase.Execute(request);
 
@@ -26,15 +27,14 @@ public class DoLoginUseCaseTest
         result.Name.ShouldBe(user.Name);
         result.Token.ShouldNotBeNullOrWhiteSpace();
     }
-
+    
     [Fact]
     public async Task Error_User_Not_Found()
     {
         var user = UserBuilder.Build();
-
         var request = RequestLoginJsonBuilder.Build();
 
-        var useCase = CreateUseCase(user);
+        var useCase = CreateUseCase(user, request.Password);
 
         var exception = await Should.ThrowAsync<InvalidLoginException>(() =>
             useCase.Execute(request)
@@ -42,13 +42,14 @@ public class DoLoginUseCaseTest
 
         exception.GetErrors().ShouldHaveSingleItem().ShouldBe(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID);
     }
-
+    
     [Fact]
     public async Task Error_Password_Not_Match()
     {
         var user = UserBuilder.Build();
 
         var request = RequestLoginJsonBuilder.Build();
+        request.Email = user.Email;
 
         var useCase = CreateUseCase(user);
 
@@ -58,10 +59,10 @@ public class DoLoginUseCaseTest
 
         exception.GetErrors().ShouldHaveSingleItem().ShouldBe(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID);
     }
-
-    private DoLoginUseCase CreateUseCase(CashFlow.Domain.Entities.User user)
+    
+    private DoLoginUseCase CreateUseCase(CashFlow.Domain.Entities.User user, string? password = null)
     {
-        var passwordEncripter = PasswordEncripterBuilder.Build();
+        var passwordEncripter = new PasswordEncrypterBuilder().Verify(password).Build();
         var tokenGenerator = JwtTokenGeneratorBuilder.Build();
         var readRepository = new UserReadOnlyRepositoryBuilder().GetUserByEmail(user).Build();
 
