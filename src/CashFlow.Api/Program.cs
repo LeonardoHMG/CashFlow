5b27dd0e553fab2e34407012290cfd7bd1ce493d
Leonardo <leonardohmgussi@gmail.com>
2026-05-16 14:04:58 -0300

    tests: starting integration tests

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -93,3 +93,5 @@ async Task MigrateDatabase()
 
     await DataBaseMigration.MigrateDataBase(scope.ServiceProvider);
 }
+
+public partial class Program { } // Make the Program class public for testing purposes
