48e9be65a1e37e3380ed595d0c1ae57ea9b100da
Leonardo <leonardohmgussi@gmail.com>
2026-05-31 11:37:21 -0300

    feat: get token from request

---
+++ src/CashFlow.Api/Token/HttpContextTokenValue.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Domain.Security.Tokens;
+using System.Net;
+
+namespace CashFlow.Api.Token;
+
+public class HttpContextTokenValue : ITokenProvider
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public HttpContextTokenValue(IHttpContextAccessor httpContextAccessor)
+    {
+        _contextAccessor = httpContextAccessor;
+    }
+
+    public string TokenOnRequest()
+    {
+        var authorization = _contextAccessor.HttpContext!.Request.Headers.Authorization.ToString();
+
+        return authorization["Bearer ".Length..].Trim(); // Remove "Bearer " prefix and trim any whitespace
+    }
+}
