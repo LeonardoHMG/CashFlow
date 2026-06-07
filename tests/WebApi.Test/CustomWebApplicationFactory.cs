c36ae60f935d2c657b5bd3242fa4f7d2f2856d7a
Leonardo <leonardohmgussi@gmail.com>
2026-06-07 12:26:12 -0300

    test: integration test for register expense

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +12,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     private CashFlow.Domain.Entities.User _user;
     private string _password;
+    private string _token;
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -28,8 +30,11 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                var scope = services.BuildServiceProvider().CreateScope();
                var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
                var passwordEncripter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
-
+             
                StartDatabase(dbContext, passwordEncripter);
+
+               var tokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
+               _token = tokenGenerator.Generate(_user);
            });
     }
 
@@ -37,6 +42,8 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     public string GetEmail() => _user.Email;
 
     public string GetPassword() => _password;
+
+    public string GetToken() => _token;
 
     private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
     {
