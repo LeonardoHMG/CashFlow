3aaf1bf9cb25bcb5e93d3379d4abe39ff9c3bce5
Leonardo <leonardohmgussi@gmail.com>
2026-06-13 11:11:12 -0300

    tests: tests for get all expenses

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,7 +1,9 @@
-﻿using CashFlow.Domain.Security.Cryptography;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -47,13 +49,26 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
     private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
     {
+        AddUsers(dbContext, passwordEncripter);
+        AddExpenses(dbContext, _user);
+
+        dbContext.SaveChanges();
+    }
+
+    private void AddUsers(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
+    {
         _user = UserBuilder.Build();
         _password = _user.Password;
 
         _user.Password = passwordEncripter.Encrypt(_user.Password);
 
         dbContext.Users.Add(_user);
+    }
 
-        dbContext.SaveChanges();
+    private void AddExpenses(CashFlowDbContext dbContext, User user)
+    {
+        var expense = ExpenseBuilder.Build(user);
+
+        dbContext.Expenses.Add(expense);
     }
 }
