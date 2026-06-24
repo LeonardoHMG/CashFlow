94ddfea0f83fd7cd8f0a9e6cec8b25697d512c1b
Leonardo <leonardohmgussi@gmail.com>
2026-06-24 11:06:28 -0300

    tests: adding test for update user profile

---
+++ tests/Validators.Tests/Users/Update/UpdateUserValidatorTest.cs
@@ -0,0 +1,65 @@
+﻿using CashFlow.Application.UseCases.Users.Update;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using Shouldly;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Validators.Tests.Users.Update;
+public class UpdateUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new UpdateUserValidator();
+        var request = RequestUpdateUserJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(null)]
+    public void Error_Name_Empty(string name)
+    {
+        var validator = new UpdateUserValidator();
+        var resquest = RequestUpdateUserJsonBuilder.Build();
+        resquest.Name = name;
+
+        var result = validator.Validate(resquest);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.NAME_EMPTY);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(null)]
+    public void Error_Email_Empty(string email)
+    {
+        var validator = new UpdateUserValidator();
+        var resquest = RequestUpdateUserJsonBuilder.Build();
+        resquest.Email = email;
+
+        var result = validator.Validate(resquest);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.EMAIL_EMPTY);
+    }
+
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        var validator = new UpdateUserValidator();
+        var resquest = RequestUpdateUserJsonBuilder.Build();
+        resquest.Email = "leonardo.com";
+
+        var result = validator.Validate(resquest);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.EMAIL_INVALID);
+    }
+}
