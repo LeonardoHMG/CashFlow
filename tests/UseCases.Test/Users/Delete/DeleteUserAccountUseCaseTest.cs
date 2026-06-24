48ead1aee3a4338f04da97fe47f110b3a5115c5d
Leonardo <leonardohmgussi@gmail.com>
2026-06-24 15:06:04 -0300

    tests: adding tests for delete user profile

---
+++ tests/UseCases.Test/Users/Delete/DeleteUserAccountUseCaseTest.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Application.UseCases.Users.Delete;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using Shouldly;
+
+namespace UseCases.Test.Users.Delete;
+public class DeleteUserAccountUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUserCase(user);
+
+        var act = async () => await useCase.Execute();
+
+        await act.ShouldNotThrowAsync();
+    }
+
+    private DeleteUserAccountUseCase CreateUserCase(User user)
+    {
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        return new DeleteUserAccountUseCase(loggedUser, repository, unitOfWork);
+    }
+}
